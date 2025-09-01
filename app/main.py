74a2e83aed59ec5225dd144bdea4d66fac129ec8
Vplusnew <fatahalim245@gmail.com>
2025-09-01 17:11:16 +0700

    update anggota api get

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
-from app.routers import anggota_router
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+from typing import List
+from app.database import SessionLocal, engine, Base
+from app.models.anggota import Anggota
+from app.schemas.anggota import AnggotaOut
+
 
 app = FastAPI(title="API Komunitas")
 
@@ -9,3 +14,31 @@ app.include_router(anggota_router.router)
 @app.get("/")
 def root():
     return {"message": "API Komunitas 🚀"}
+
+
+from fastapi import FastAPI
+from app.routers import anggota_router
+
+app = FastAPI(title="API Komunitas")
+
+app.include_router(anggota_router.router)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def root():
+    return {"message": "API Komunitas 🚀"}
+
+
+# Endpoint GET semua anggota
+@app.get("/anggota", response_model=List[AnggotaOut])
+def get_all_anggota(db: Session = Depends(get_db)):
+    anggota_list = db.query(Anggota).all()
+    return anggota_list
