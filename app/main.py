ea90c51a28a5d4ade7774b84d527a310c2d4041a
Vplusnew <fatahalim245@gmail.com>
2025-09-01 17:37:34 +0700

    updatemain

--- app/main.py
+++ app/main.py
@@ -13,19 +13,6 @@ app = FastAPI(title="API Komunitas")
 app.include_router(anggota_router.router)
 
 
-@app.get("/")
-def root():
-    return {"message": "API Komunitas 🚀"}
-
-
-from fastapi import FastAPI
-from app.routers import anggota_router
-
-app = FastAPI(title="API Komunitas")
-
-app.include_router(anggota_router.router)
-
-
 def get_db():
     db = SessionLocal()
     try:
